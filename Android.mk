7347b39a09731cbed82da47e00dec9ecf779e295
Deivid21 <david.parra.ignacio@gmail.com>
2025-09-19 23:46:34 -0300

    tundra: Move radio to external repository
    * Moved to: "https://gitlab.com/Deivid-21s-Sources/proprietary_vendor_motorola_tundra-radio.git"
    
    Change-Id: I4ca1b8eac7812fa442dc5b90b4ba2f6991087fa0

--- Android.mk
+++ Android.mk
@@ -1,29 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),tundra)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,a185605916a365df7ca6104179cd8b1b064499c0)
-$(call add-radio-file-sha1-checked,radio/aop.img,c7a78506e92b89fc251df97a59e3de84418d0d66)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,48a39e6be8148d977e8951185f16c1daca723f24)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,ff19bd298bbf6ac4ad120ed61a961cd27d11cdbc)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,2891f141b88a486fdbb55b7179c409640910bf1e)
-$(call add-radio-file-sha1-checked,radio/dsp.img,905ecf44147441699bcaffe4dded13b02dc35b4a)
-$(call add-radio-file-sha1-checked,radio/fsg.img,2540811b35f5c20302ba023973e3a12dc46d4228)
-$(call add-radio-file-sha1-checked,radio/hyp.img,2e8e85e65e41ecd8ef42309d7aa72505f0ca82c8)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,6415aa6cfe38e631b2088ada76a9805c6ba29ba8)
-$(call add-radio-file-sha1-checked,radio/logo.img,15e867893c807995ae6834fc97423b23b8f68501)
-$(call add-radio-file-sha1-checked,radio/modem.img,765fa38d1f355a474ce473020841921517376140)
-$(call add-radio-file-sha1-checked,radio/prov.img,bae04b7c34b51cae82f9b35211a539957d222e3a)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,1c682db097a7fccddb1b6e806429af6fbdb9e880)
-$(call add-radio-file-sha1-checked,radio/shrm.img,fb36f7d7b1ad0464ac90b879d4c0c795aab15822)
-$(call add-radio-file-sha1-checked,radio/storsec.img,bad404af8628da8a53c5083d93795d361c37e944)
-$(call add-radio-file-sha1-checked,radio/tz.img,57af21acf7e47aeeca12a0aab334cc5628541fd6)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,6cb4c858cb201a4da697c97ab53d569991a1311a)
-$(call add-radio-file-sha1-checked,radio/xbl.img,5eaa19182aa78c4fc8fcc0e336611775f07a572c)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,f680c1647587ca47a773f29837e32e111afd0e0c)
-
-endif
